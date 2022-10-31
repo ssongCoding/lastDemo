1cc6085071ace2b6c07ba4e38d745dd17bb7ba1e
kimyonghee1216 <kimyh1216@kakao.com>
2022-10-31 16:42:26 +0900

    Controller 구현

--- src/main/java/com/example/lastDemo/Member/MemberController.java
+++ src/main/java/com/example/lastDemo/Member/MemberController.java
@@ -1,5 +1,35 @@
 package com.example.lastDemo.Member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
 
+    // 로그인
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // 회원가입
+    @GetMapping("/join")
+    public String join() {
+        return "signup";
+    }
+
+    // 마이페이지
+    @PostMapping("/mypage")
+    public String mypage() {
+        return "mypage";
+    }
+    
+    // 웰컴페이지
+    @PostMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
 }
--- src/main/java/com/example/lastDemo/Product/ProductController.java
+++ src/main/java/com/example/lastDemo/Product/ProductController.java
@@ -1,4 +1,29 @@
 package com.example.lastDemo.Product;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+
+    // 메인페이지 main.html get /product
+    @GetMapping
+    public String main() {
+        return "main";
+    }
+
+    // 메인페이지 main.html post /product
+    @PostMapping
+    public String logonmain() {
+        return "main";
+    }
+
+    // 상품조회 search.html get /product/search
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
 }
